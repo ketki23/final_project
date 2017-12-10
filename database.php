67d1a525f93ddc77e22c8c8402baaed73110963a
Ketki R Andhale <ketki.andhale@gmail.com>
2017-12-09 22:44:01 -0500

    first commit

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DATABASE', 'sd686');
-define('USERNAME', 'sd686');
-define('PASSWORD', 'ZOm1EN5l3');
-define('CONNECTION', 'sql1.njit.edu');
+define('DATABASE', 'kra23');
+define('USERNAME', 'kra23');
+define('PASSWORD', 'z4QhaWbRd');
+define('CONNECTION', 'sql2.njit.edu');
